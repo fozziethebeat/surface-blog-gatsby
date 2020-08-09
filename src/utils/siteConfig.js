9ce4a0ace1695bf8deea5c8c764dc04a63b17af4
Keith Stevens <fozziethebeat@gmail.com>
2020-08-09 18:44:50 +0900

    Adding first step of gatsby version of surface blog

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteUrl : `http://blog-dev.surface-coop.com`,
+  postsPerPage : 12,
+  siteTitleMeta : `Surface Coop`,
+  siteDescriptionMeta : `Documenting the journey of making a tiny coop.`,
+  shareImageWidth : 1000,
+  shareImageHeight : 523,
+  shortTitle : `Surface Coop`,
+  siteIcon : `favicon.png`,
+  backgroundColor : `#e9e9e9`,
+  themeColor : `#15171A`,
+}
